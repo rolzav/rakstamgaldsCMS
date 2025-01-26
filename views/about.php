22da6e58fc159f830c038c8d70d8c6f81b939cac
Kakjis <80541682+rolzav@users.noreply.github.com>
2025-01-27 00:58:26 +0200

    Create about.php

---
+++ views/about.php
@@ -0,0 +1,6 @@
+<?php include __DIR__ . '/partials/header.php'; ?>
+<div class="container">
+    <h1><?php echo htmlspecialchars($title, ENT_QUOTES, 'UTF-8'); ?></h1>
+    <p>This is the about page.</p>
+</div>
+<?php include __DIR__ . '/partials/footer.php'; ?>
