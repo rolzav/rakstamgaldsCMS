f925f922af7ff431e1f7128e5434a0f10968c76f
Kakjis <80541682+rolzav@users.noreply.github.com>
2025-01-27 00:08:18 +0200

    Create home.php

---
+++ views/home.php
@@ -0,0 +1,6 @@
+<?php include __DIR__ . '/partials/header.php'; ?>
+<div class="container">
+    <h1><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></h1>
+    <p>Welcome to the Blog CMS!</p>
+</div>
+<?php include __DIR__ . '/partials/footer.php'; ?>
