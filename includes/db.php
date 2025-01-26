da1f1bb249fd018c88fdc8bd048c454a41979df6
Kakjis <80541682+rolzav@users.noreply.github.com>
2025-01-27 00:05:16 +0200

    Create db.php

---
+++ includes/db.php
@@ -0,0 +1,38 @@
+<?php
+class Database
+{
+    private static $instance = null;
+    private $connection;
+
+    private function __construct() {
+        $config = require __DIR__ . '/../config/config.php';
+        $database = $config['db']['database'];
+
+        $dsn = "sqlite:$database";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            $this->connection = new PDO($dsn, null, null, $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
+?>
