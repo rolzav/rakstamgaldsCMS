9e837e3bcaf3bc3bf8b64b0813880ce6917b4b19
Kakjis <80541682+rolzav@users.noreply.github.com>
2025-01-27 01:00:15 +0200

    Create 404.php

---
+++ views/404.php
@@ -0,0 +1,7 @@
+<?php include __DIR__ . '/partials/header.php'; ?>
+<div class="container">
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <a href="/" class="btn btn-primary">Go to Home</a>
+</div>
+<?php include __DIR__ . '/partials/footer.php'; ?>
